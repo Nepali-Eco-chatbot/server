e3a3cfb8c52146d3d2b490c9afc6632bdd78c50c
Praj2064 <prajwalbhandari567@gmail.com>
2026-07-24 11:24:45 +0545

    Remove render route and renderer usage, keep webhook only

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,22 @@
 import { Hono } from "hono";
-import { renderer } from "./renderer";
 import { env } from "hono/adapter";
 
 const app = new Hono();
-
-app.use(renderer);
-
-app.get("/", (c) => {
-  return c.render(
-    <div>
-      <h1>Hello Team! </h1>
-      <p>Nepali Eco Chatbot</p>
-    </div>,
-  );
-});
 
 app.get("/webhook", (c) => {
   // verification token sent by wp api
   const verificationToken = c.req.query("hub.verify_token");
   const challenge = c.req.query("hub.challenge");
-
   const { META_WP_API_VERIFICATION_TOKEN: server_verification_token } = env<{
     META_WP_API_VERIFICATION_TOKEN: string;
   }>(c);
-
   if (!server_verification_token) throw new Error("Server Env not found");
   if (!verificationToken)
     throw new Error("Meta did not send verification token");
   if (!challenge) throw new Error("Meta did not send any challenge");
-
   if (verificationToken === server_verification_token) {
     return c.text(challenge, 200);
   }
-
   return c.text("Invalid verification token", 401);
 });
 
